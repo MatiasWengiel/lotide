e6877f617938e241d13280b1d6713ad533023ad1
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-24 08:45:07 -0500

    Added file that returns the tail of an array

---
+++ tail.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}. ✅`);
+  } else {
+    console.log(`🚩 Assertion Failed: ${actual} !== ${expected}. 🚩`);
+  }
+};
+
+const tail = function(array) {
+  if (Array.isArray(array)) {
+    return array.length > 1 ? array.slice(1) : [];
+  } else {
+    return "This argument is not an array";
+  }
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+assertEqual(tail(words).length, words.length - 1);
+assertEqual(tail(words)[0], words[1]);
+assertEqual(tail(words)[1], words[2]);
+assertEqual(words.length, 3);
+
+const oneWord = ["One"];
+assertEqual(tail(oneWord).length, 0); // an empty array has a length of zero
+assertEqual(Array.isArray(tail(oneWord)), true); // making sure it still returns an array
+assertEqual(oneWord.length, 1);
+
+const noWord = [];
+assertEqual(tail(noWord).length, 0); // an empty array has a length of zero
+assertEqual(Array.isArray(tail(noWord)), true); // making sure it still returns an array
+assertEqual(noWord.length, 0);
+
+const noArray = "";
+assertEqual(tail(noArray), "This argument is not an array");
