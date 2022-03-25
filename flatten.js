a2568c142b082109ff533a0b5d8c9d6bfc88bbd5
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-25 15:19:12 -0500

    added flatten.js which takes an array with arrays inside of it and returns all of the values in a single array

---
+++ flatten.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arrayOne, arrayTwo){
+  if (arrayOne.length !== arrayTwo.length){
+    return false
+  }
+
+  for (let i = 0; i < arrayOne.length; i++){
+    if (arrayOne[i] !== arrayTwo[i]){
+      return false
+    } else {
+      return true
+    }
+  }
+}
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+
+  eqArrays(arrayOne, arrayTwo) ? console.log(`✅ Assertion Passed: ${arrayOne} === ${arrayTwo}. ✅`) : 
+  console.log(`🚩 Assertion Failed: ${arrayOne} !== ${arrayTwo}. 🚩`);
+   
+}
+
+const flatten = function(array){
+  let flattenedArray = [];
+  for (let i = 0; i < array.length; i++){
+    if (!(Array.isArray(array[i]))){
+      flattenedArray.push(array[i])
+      } else {
+        for (item of array[i]) {
+          flattenedArray.push(item);
+      }
+    }
+  }
+  return flattenedArray;
+}
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten(["fox", ["bear", "rabbit"], "elephant"]), ["fox", "bear", "rabbit", "elephant"]);
+assertArraysEqual(flatten([2, 3, 4]), [2, 3, 4]);
+assertArraysEqual(flatten([2, "fox", true, [true, false], "bear"]), [2, "fox", true, true, false, "bear"]);
