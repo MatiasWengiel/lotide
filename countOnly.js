4e084b0ab8f8e1a83a7dd3fda53d5b642034fc76
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-28 13:07:46 -0500

    The function runs even if booleans or numbers are added into the array of allItems (and ignores them, as it should)

--- countOnly.js
+++ countOnly.js
@@ -8,6 +8,10 @@ const assertEqual = function(actual, expected) {
 
 const countOnly = function(allItems, itemsToCount) {
   let countedItems = {};
+  // For each key in itemsToCount IF they are true, we iterate through eachItem of allItems and compare it to
+  // the truthy item in ItemsToCount. If they match we check to see if there's already a key with that item's 
+  // name inside of countedItems. If so, we add 1 to the count. If not, we create it with a starting value
+  // of 1
   for (item in itemsToCount) {
     if (itemsToCount[item]) { 
       for (eachItem of allItems) {
@@ -32,7 +36,12 @@ const firstNames = [
   "Jason",
   "Salima",
   "Fang",
-  "Joe"
+  "Joe",
+  1,
+  true,
+  false,
+  "true",
+  "false",
 ];
 const testresult = countOnly(firstNames)
 
