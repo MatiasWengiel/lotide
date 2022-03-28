7c90588870814cf2dd7149a805eba692a8fad0ad
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-28 15:24:26 -0500

    Beginning stages, spaces from the original argument are removed and capital letters turned to lower case. Indicated logic of indexFinder function

---
+++ letterPositions.js
@@ -0,0 +1,13 @@
+const letterPositions = function (sentence) {
+  const lowerCaseNoSpaces = sentence.split(" ").join("").toLowerCase();
+  console.log(lowerCaseNoSpaces);
+  const results = {};
+
+  const indexFinder = function (lowerCaseNoSpaces) {
+    // will find the index of a letter, then push it to results along with each index in which the letter occurs"
+  }
+
+  return results;
+}
+
+console.log(letterPositions("Hello world"));
