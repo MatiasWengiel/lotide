2713626a3836cdb04c205f163ec716004931a64f
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-28 16:17:49 -0500

    Created a working prototype of the function and manually tested it

--- letterPositions.js
+++ letterPositions.js
@@ -1,13 +1,53 @@
+const eqArrays = function(arrayOne, arrayTwo){
+  if (arrayOne.length !== arrayTwo.length){
+    return false
+  }
+
+  if (arrayOne.length === 0 && arrayTwo.length === 0) {
+    return true
+  }
+
+  for (let i = 0; i < arrayOne.length; i++){
+    if (arrayOne[i] !== arrayTwo[i]){
+      return false
+    } 
+    
+  }
+  return true
+}
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+
+  eqArrays(arrayOne, arrayTwo) ? console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${arrayOne} === ${arrayTwo}. ✅`) : 
+  console.log("\x1b[31m%s\x1b[0m", `🚩 Assertion Failed: ${arrayOne} !== ${arrayTwo}. 🚩`);
+   
+}
+
+
 const letterPositions = function (sentence) {
   const lowerCaseNoSpaces = sentence.split(" ").join("").toLowerCase();
-  console.log(lowerCaseNoSpaces);
   const results = {};
 
-  const indexFinder = function (lowerCaseNoSpaces) {
-    // will find the index of a letter, then push it to results along with each index in which the letter occurs"
+  //Iterate through lowerCaseNoSpaces
+  //Check each letter in lowerCaseNoSpaces to see if it's present in results
+  // if it is not, create an array with the key of the letter at i and the value of i
+  // if it is, push i to the existing array
+  for (let i = 0; i < lowerCaseNoSpaces.length; i++){
+    if (results[lowerCaseNoSpaces[i]]) {
+      results[lowerCaseNoSpaces[i]].push(i);
+  } else {
+    results[lowerCaseNoSpaces[i]] = [i]
   }
+}
 
   return results;
 }
 
 console.log(letterPositions("Hello world"));
+assertArraysEqual(letterPositions("Hello world").h, [0]);
+assertArraysEqual(letterPositions("Hello world").e, [1])
+assertArraysEqual(letterPositions("Hello world").l, [2, 3, 8]);
+assertArraysEqual(letterPositions("Hello world").o, [4, 6]);
+assertArraysEqual(letterPositions("Hello world").w, [5]);
+assertArraysEqual(letterPositions("Hello world").r, [7]);
+assertArraysEqual(letterPositions("Hello world").d, [9]);
