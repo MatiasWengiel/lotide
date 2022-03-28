b472262a1bd677c7000f4041cef23be950e18149
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-28 15:17:18 -0500

    ran eslint on file and corrected small errors

--- countLetters.js
+++ countLetters.js
@@ -5,7 +5,7 @@ const countLetters = function(string) {
     
   }
   // remove spaces and capital letters to get an accurate count
-  let lowerCaseNoSpaces = string.split(" ").join("").toLowerCase(); 
+  let lowerCaseNoSpaces = string.split(" ").join("").toLowerCase();
   let letterCount = {};
   for (let eachLetter of lowerCaseNoSpaces) {
     letterCount[eachLetter] ? letterCount[eachLetter] += 1 : letterCount[eachLetter] = 1;
@@ -18,4 +18,4 @@ console.log(countLetters("Taco cat"));
 console.log(countLetters("Matias Wengiel"));
 console.log(countLetters(true));
 console.log(countLetters(undefined));
-console.log(countLetters(123)); 
+console.log(countLetters(123));
