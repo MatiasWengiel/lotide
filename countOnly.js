d9bfee024a0affa02416710dcacddff7b3460236
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-28 12:36:03 -0500

    Starting stage of countOnly, and practicing committing to github as I'm working

---
+++ countOnly.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected}. ✅`);
+  } else {
+    console.log("\x1b[31m%s\x1b[0m",`🚩 Assertion Failed: ${actual} !== ${expected}. 🚩`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let countedItems = {};
+  for (oneName of allItems){
+    console.log(oneName);
+  }
+}
+
+
+//testing code below
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+const testresult = countOnly(firstNames)
+/*
+const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined); */
