d500d636e8873536e08557836a7480b6a1d6a0aa
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-28 16:56:53 -0500

    I need to remember to run eslint before my final commit. I did it now.

--- letterPositions.js
+++ letterPositions.js
@@ -1,32 +1,32 @@
-const eqArrays = function(arrayOne, arrayTwo){
-  if (arrayOne.length !== arrayTwo.length){
-    return false
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
   }
 
   if (arrayOne.length === 0 && arrayTwo.length === 0) {
-    return true
+    return true;
   }
 
-  for (let i = 0; i < arrayOne.length; i++){
-    if (arrayOne[i] !== arrayTwo[i]){
-      return false
-    } 
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
     
   }
-  return true
-}
+  return true;
+};
 
 const assertArraysEqual = function(arrayOne, arrayTwo) {
 
-  eqArrays(arrayOne, arrayTwo) ? console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${arrayOne} === ${arrayTwo}. ✅`) : 
-  console.log("\x1b[31m%s\x1b[0m", `🚩 Assertion Failed: ${arrayOne} !== ${arrayTwo}. 🚩`);
+  eqArrays(arrayOne, arrayTwo) ? console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${arrayOne} === ${arrayTwo}. ✅`) :
+    console.log("\x1b[31m%s\x1b[0m", `🚩 Assertion Failed: ${arrayOne} !== ${arrayTwo}. 🚩`);
    
-}
+};
 
 
-const letterPositions = function (sentence) {
-  if (typeof sentence !== "string") { //ensure we are dealing with a string
-    return console.log("Error: Argument is not a string"); 
+const letterPositions = function(sentence) {
+  if (typeof string !== "string") { //ensure we are dealing with a string
+    return "Error: Argument is not a string";
   }
   const lowerCaseNoSpaces = sentence.split(" ").join("").toLowerCase();
   const results = {};
@@ -35,19 +35,18 @@ const letterPositions = function (sentence) {
   //Check each letter in lowerCaseNoSpaces to see if it's present as a key in results
   // if it is not, create an array with the key of the letter at i and the value of i
   // if it is, push i to the existing array
-  for (let i = 0; i < lowerCaseNoSpaces.length; i++){
+  for (let i = 0; i < lowerCaseNoSpaces.length; i++) {
     if (results[lowerCaseNoSpaces[i]]) {
       results[lowerCaseNoSpaces[i]].push(i);
-  } else {
-    results[lowerCaseNoSpaces[i]] = [i]
+    } else {
+      results[lowerCaseNoSpaces[i]] = [i];
+    }
   }
-}
-
   return results;
-}
+};
 
 assertArraysEqual(letterPositions("Hello world").h, [0]);
-assertArraysEqual(letterPositions("Hello world").e, [1])
+assertArraysEqual(letterPositions("Hello world").e, [1]);
 assertArraysEqual(letterPositions("Hello world").l, [2, 3, 8]);
 assertArraysEqual(letterPositions("Hello world").o, [4, 6]);
 assertArraysEqual(letterPositions("Hello world").w, [5]);
