2303a5790ea470b86b5a7abb45ad259586691bb1
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-28 15:07:11 -0500

    Finished letter counter. May have forgotten to commit partial work

--- countLetters.js
+++ countLetters.js
@@ -1,17 +1,21 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected}. ✅`);
-  } else {
-    console.log("\x1b[31m%s\x1b[0m",`🚩 Assertion Failed: ${actual} !== ${expected}. 🚩`);
-  }
-};
-
 const countLetters = function(string) {
-  let lowerCaseNoSpaces = string.split(" ").join("").toLowerCase();
-  console.log(lowerCaseNoSpaces);
-  for (let letter of lowerCaseNoSpaces) {
-    console.log(letter);
+  
+  if (typeof string !== "string") { //ensure we are dealing with a string
+    return console.log("Error: Argument is not a string");
+    
   }
+  // remove spaces and capital letters to get an accurate count
+  let lowerCaseNoSpaces = string.split(" ").join("").toLowerCase(); 
+  let letterCount = {};
+  for (let eachLetter of lowerCaseNoSpaces) {
+    letterCount[eachLetter] ? letterCount[eachLetter] += 1 : letterCount[eachLetter] = 1;
+  }
+  return letterCount;
 };
 
-countLetters("Taco cat");
+//Testing manually, since assertEqual (which is suggested on compass) would not properly test objects
+console.log(countLetters("Taco cat"));
+console.log(countLetters("Matias Wengiel"));
+console.log(countLetters(true));
+console.log(countLetters(undefined));
+console.log(countLetters(123)); 
