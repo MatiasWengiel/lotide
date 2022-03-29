1bbcf3dab247c965f9688e00806036e2473a7d3d
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-29 15:41:07 -0500

    Working version that passes more tests, with a second solution based on the suggestion in Compass as well

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,19 +6,34 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const findKeyByValue = function (object, value) {
+const findKeyByValue = function(object, value) {
   for (let key in object) {
-     if (object[key] === value) {
-       return key
-     }
+    if (object[key] === value) {
+      return key;
+    }
   }
 };
-
-const bestTVShowsByGenre = { 
+/* I opened the "Spoilers" on Compass and saw that using Object.keys and for..of was suggested, so I did that too. However, I am keeping my original solution since it works and skips the step of having to extract the keys from the object into an array and then iterate through them anyway.
+const findKeyByValue = function(object, value) {
+  let arrayOfKeys = Object.keys(object);
+  for (key of arrayOfKeys){
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+*/
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
+  drama:  "The Wire",
+  "99": "Brooklyn",
+  nothing: undefined,
+  "horror-coding": "the infinite loop that can't be escaped"
 };
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, undefined), "nothing");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn"), "99");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "the infinite loop that can't be escaped"), "horror-coding");
