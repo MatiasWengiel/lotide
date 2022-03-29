dbf7da31f5070ba4bb370d3fbc153ee829895f63
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-29 15:09:15 -0500

    Starting point of the findKeyByValue, a simple function that iterates through an object and prints the keys

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected}. ✅`);
+  } else {
+    console.log("\x1b[31m%s\x1b[0m",`🚩 Assertion Failed: ${actual} !== ${expected}. 🚩`);
+  }
+};
+
+const findKeyByValue = function (object, value) {
+  for (key in object) {
+    // This function will iterate through the values associated with the keys and see if a value matches the input - then return the associated key. For now, it just console.logs the key as a starting point
+    console.log(key);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
