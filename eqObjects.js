08537354380e6d50051056f62eac089118524f73
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-29 16:06:06 -0500

    Created a function that compares the keys and values of two objects, but only works for objects of the same length using primitive values

--- eqObjects.js
+++ eqObjects.js
@@ -7,13 +7,24 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
-
+  let output = false
+  for (key1 in object1) {
+    for (key2 in object2) {
+      if (object1[key1] === object2[key2]) {
+        output = true;
+      }
+    }
+  }
+  return output;
 }
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
+const one = { one: "uno", two: "dos" };
+const uno = { one: "uno", two: "dos" };
 
 assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(one, uno), true);
 
 const abc = { a: "1", b: "2", c: "3" };
 
