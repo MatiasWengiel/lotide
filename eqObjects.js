063f61dcf835a4b3f43ad346b144cd78ae9f5aae
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-29 18:48:02 -0500

    eqObjects now passes extended tests with arrays and primitives as values

--- eqObjects.js
+++ eqObjects.js
@@ -26,35 +26,34 @@ const eqArrays = function(arrayOne, arrayTwo) {
 const eqObjects = function(object1, object2) {
 /*  if (Object.keys(object1).length !== Object.keys(object2).length) { //If they aren't the same length, they aren't identical
     return false
-  } 
-  // would it be better to keep the check above, since it can quickly rule out cases of different lengths? 
+  }
+  // would it be better to keep the check above, since it can quickly rule out cases of different lengths?
   */
 
-  for (key1 in object1) { //first check to see if object two has all of the same keys in object 1
-    if (object2[key1] === undefined) { 
-      return false; 
+  for (let key1 in object1) { //first check to see if object two has all of the same keys in object 1
+    if (object2[key1] === undefined) {
+      return false;
     }
   }
 
-  for (key2 in object2) {
+  for (let key2 in object2) {
     if (object1[key2] === undefined) { //also check to see if object1 has all of the same keys in object2
       return false;
     }
   }
-// The two comparisons above will also return false if the lengths are different (as they will have an unmatched key) 
-  for (key1 in object1){
-      console.log(Array.isArray(object1[key1]));
-      if (Array.isArray(object1[key1])) {
-       if (eqArrays(object1[key1], object2[key1]) === false ){
-         return false;
-       }
-      } else if (object1[key1] !== object2[key1]) { //If we get to this point, check to see if the values in the keys match
+  // The two comparisons above will also return false if the lengths are different (as they will have an unmatched key)
+  for (let key1 in object1) {
+    if (Array.isArray(object1[key1])) {
+      if (eqArrays(object1[key1], object2[key1]) === false) {
         return false;
-      } 
+      }
+    } else if (object1[key1] !== object2[key1]) { //If we get to this point, check to see if the values in the keys match
+      return false;
     }
+  }
   return true; //If we get here without escaping the function in a previous return, the objects are a match
-}
-/* Tests below passed
+};
+
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const bc = { b: "2", c: "3" };
@@ -80,11 +79,25 @@ assertEqual(eqObjects(numbersNow, moreWrongNumbers), false);
 const abc = { a: "1", b: "2", c: "3" };
 
 assertEqual(eqObjects(ab, abc), false);
-*/
+
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 assertEqual(eqObjects(cd, dc), true);
 
-cd2 = { c: "1", d: ["2", 3, 4] };
+const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+const twoArrays = { a: [1, 2], b: [3, 4] };
+const twoArraysAgain = { b: [3, 4], a: [1, 2] };
+const twoArraysFalse = { b: [3, 4], a: [1, 2, 3] };
+const twoArraysFalse2 = { b: [3, 4], a: [2, 3] };
+const stringInArray = { a: ["string", "strung"], b: 1 };
+const stringInArrayAgain = { b: 1, a: ["string", "strung"] };
+const arrayWithBooleans = { a: [true, false], b: false };
+const arrayWithBooleansAgain = { b: false, a: [true, false] };
+
+assertEqual(eqObjects(twoArrays, twoArraysAgain), true);
+assertEqual(eqObjects(twoArraysAgain, twoArraysFalse), false);
+assertEqual(eqObjects(twoArrays, twoArraysFalse2), false);
+assertEqual(eqObjects(stringInArray, stringInArrayAgain), true);
+assertEqual(eqObjects(arrayWithBooleans, arrayWithBooleansAgain), true);
 
