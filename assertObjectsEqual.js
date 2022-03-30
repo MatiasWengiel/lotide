6cb85ed5e923549d3495f89ac3bef95da2007976
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-29 21:32:50 -0500

    Function now compares two objects and checks to see if they are equal, then correctly prints the objects in the console.log statement

---
+++ assertObjectsEqual.js
@@ -0,0 +1,98 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+
+  if (arrayOne.length === 0 && arrayTwo.length === 0) {
+    return true;
+  }
+
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+/*  if (Object.keys(object1).length !== Object.keys(object2).length) { //If they aren't the same length, they aren't identical
+    return false
+  }
+  // would it be better to keep the check above, since it can quickly rule out cases of different lengths?
+  */
+
+  for (let key1 in object1) { //first check to see if object two has all of the same keys in object 1
+    if (object2[key1] === undefined) {
+      return false;
+    }
+  }
+
+  for (let key2 in object2) {
+    if (object1[key2] === undefined) { //also check to see if object1 has all of the same keys in object2
+      return false;
+    }
+  }
+  // The two comparisons above will also return false if the lengths are different (as they will have an unmatched key)
+  for (let key1 in object1) {
+    if (Array.isArray(object1[key1])) {
+      if (eqArrays(object1[key1], object2[key1]) === false) {
+        return false;
+      }
+    } else if (object1[key1] !== object2[key1]) { //If we get to this point, check to see if the values in the keys match
+      return false;
+    }
+  }
+  return true; //If we get here without escaping the function in a previous return, the objects are a match
+};
+
+const assertObjectsEqual = function(object1, object2) {
+  const inspect = require('util').inspect;
+
+  eqObjects(object1, object2) ? console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${inspect(object1)} === ${inspect(object2)}. ✅`) :
+    console.log("\x1b[31m%s\x1b[0m", `🚩 Assertion Failed: ${inspect(object1)} !== ${inspect(object2)}. 🚩`);
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const bc = { b: "2", c: "3" };
+const badab = { a: "2", b: "1" };
+const isTruth = { a: true, b: true};
+const stillTruth = { a: true, b: true };
+const isFalse = { a: true, b: false };
+const numbersNow = { a: 1, b: 2 };
+const numbersStill = { b: 2, a: 1 };
+const numbersWrong = { a: 2, b: 1 };
+const moreWrongNumbers = { a: 1, b: 2, c: 3 };
+const abc = { a: "1", b: "2", c: "3" };
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+const twoArrays = { a: [1, 2], b: [3, 4] };
+const twoArraysAgain = { b: [3, 4], a: [1, 2] };
+const twoArraysFalse = { b: [3, 4], a: [1, 2, 3] };
+const twoArraysFalse2 = { b: [3, 4], a: [2, 3] };
+const stringInArray = { a: ["string", "strung"], b: 1 };
+const stringInArrayAgain = { b: 1, a: ["string", "strung"] };
+const arrayWithBooleans = { a: [true, false], b: false };
+const arrayWithBooleansAgain = { b: false, a: [true, false] };
+
+console.log("true");
+assertObjectsEqual(ab, ba);
+assertObjectsEqual(isTruth, stillTruth);
+assertObjectsEqual(numbersNow, numbersStill);
+assertObjectsEqual(cd, dc);
+assertObjectsEqual(twoArrays, twoArraysAgain);
+assertObjectsEqual(stringInArray, stringInArrayAgain);
+assertObjectsEqual(arrayWithBooleans, arrayWithBooleansAgain);
+console.log("false \n");
+
+assertObjectsEqual(ba, bc);
+assertObjectsEqual(ab, badab);
+assertObjectsEqual(isTruth, isFalse);
+assertObjectsEqual(numbersNow, moreWrongNumbers);
+assertObjectsEqual(ab, abc);
+assertObjectsEqual(cd, cd2);
+assertObjectsEqual(numbersNow, numbersWrong);
+assertObjectsEqual(twoArraysAgain, twoArraysFalse);
+assertObjectsEqual(twoArrays, twoArraysFalse2);
