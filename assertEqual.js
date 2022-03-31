5cfaa7812b9134b9b6d62a4bac424d2680bb4da4
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-30 19:13:06 -0500

    Modified simple if statement to use ternary operator

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,6 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected}. ✅`);
-  } else {
+  return actual === expected ? console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected}. ✅`):
     console.log("\x1b[31m%s\x1b[0m",`🚩 Assertion Failed: ${actual} !== ${expected}. 🚩`);
-  }
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
