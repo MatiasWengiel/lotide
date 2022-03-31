35e55f3b9834c1178473bf9483939523822f5b80
Matias Wengiel <matiaswengiel@gmail.com>
2022-03-31 12:26:32 -0500

    Developed logic for accessing star rating, ignoring callback fn for now

--- findKey.js
+++ findKey.js
@@ -3,10 +3,17 @@ const assertEqual = function(actual, expected) {
     console.log("\x1b[31m%s\x1b[0m",`🚩 Assertion Failed: ${actual} !== ${expected}. 🚩`);
 };
 
-const findKey = function (object, callback) {
+const findKey = function(object, callback) {
   // for each movie name (key) in object, check the value of the key (which is a second object)
   // Within the second object, check the value of the .stars key and if it matches 2 return the original key (movie name)
-}
+  for (let key in object) {
+    for (let rating in object[key]) {
+      if (object[key][rating] === 2) {
+        return key;
+      }
+    }
+  }
+};
 
 let movies = findKey({
   "Blue Hill": { stars: 1 },
@@ -15,5 +22,5 @@ let movies = findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2); // => "noma"
 assertEqual(movies, "noma");
