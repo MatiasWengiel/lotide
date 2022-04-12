f6ddc0555d091f24cfb00e469df3d32d202c4f76
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-12 18:45:23 -0500

    Created a dedicated testing folder and tests for assertEqual and head

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual')
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("This is a string", "This is a string");
+assertEqual("This is a string", "this is a string");
+assertEqual(1, 2);
