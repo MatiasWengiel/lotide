4e5532ae93828c9fdf759968f247b9ad5b5859b6
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-12 21:07:31 -0500

    Refactored middle to export its function, and moved tests to its own testing file

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-
-  if (arrayOne.length === 0 && arrayTwo.length === 0) {
-    return true;
-  }
-
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-
-  eqArrays(arrayOne, arrayTwo) ? console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${arrayOne} === ${arrayTwo}. ✅`) :
-    console.log("\x1b[31m%s\x1b[0m", `🚩 Assertion Failed: ${arrayOne} !== ${arrayTwo}. 🚩`);
-};
-
 const middle = function(array) {
   let middleIndex = 0;
   if (array.length <= 2) {
@@ -37,11 +14,6 @@ const middle = function(array) {
     
   }
 };
-console.log(assertArraysEqual(middle([1]), []));
-console.log(assertArraysEqual(middle([1, 2]), []));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
-console.log(assertArraysEqual(middle([1, 2, 3]), [2]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
-console.log(assertArraysEqual(middle(["one", 2, true]), [2]));
-console.log(assertArraysEqual(middle(["fox", "bear", "rabbit", "orca"]), ["bear", "rabbit"]));
+
+module.exports = middle;
+
