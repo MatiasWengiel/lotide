01716521ab6caca1d01ee824fd8c4150fc82f0a0
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-13 18:54:47 -0500

    Created separate test file for countLetters function, and added a case where the function returns an error message if passed an empty string

--- countLetters.js
+++ countLetters.js
@@ -1,13 +1,11 @@
-// const assertEqual = function(actual, expected) {
-//   return actual === expected ? console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected}. ✅`) :
-//     console.log("\x1b[31m%s\x1b[0m",`🚩 Assertion Failed: ${actual} !== ${expected}. 🚩`);
-// };
-
-
 const countLetters = function(string) {
   
   if (typeof string !== "string") { //ensure we are dealing with a string
     return "Error: Argument is not a string";
+  }
+
+  if (string === "") {
+    return "Error: String has no characters within";
   }
   // remove spaces and capital letters to get an accurate count
   let lowerCaseNoSpaces = string.split(" ").join("").toLowerCase();
@@ -20,8 +18,3 @@ const countLetters = function(string) {
 
 module.exports = countLetters;
 
-// assertEqual(countLetters("Taco cat").t, 2);
-// assertEqual(countLetters("Matias Wengiel").i, 2);
-// assertEqual(countLetters("iiifiiaaa").i, 5);
-// assertEqual(countLetters(true),"Error: Argument is not a string");
-// assertEqual(countLetters(undefined), "Error: Argument is not a string");
---
+++ test/countLettersTest.js
@@ -0,0 +1,47 @@
+const assert = require('chai').assert;
+const countLetters = require('../countLetters');
+
+describe('#countLetters', () => {
+  it('should return a count of each letter in "taco cat" as an object (i.e. {t: 2, a: 2, c: 2, o: 1}', () => {
+    const input = "taco cat";
+    const expectedOutput = {t: 2, a: 2, c: 2, o: 1}
+    assert.deepEqual(countLetters(input), expectedOutput);
+  })
+
+  it('should return a proper count of each letter, even if some are capitalized. In this case "TacO CAt" should return {t: 2, a: 2, c: 2, o: 1}', () => {
+    const input = "TacO Cat";
+    const expectedOutput = {t: 2, a: 2, c: 2, o: 1};
+    assert.deepEqual(countLetters(input), expectedOutput);
+  })
+
+  it('should properly count numbers that are passed as a string. For example, "122333" should return {1:1, 2:2, 3:3}', () => {
+    const input = "122333"
+    const expectedOutput = {1:1, 2:2, 3:3}
+    assert.deepEqual(countLetters(input), expectedOutput);
+  })
+
+  it('should return "Error: Argument is not a string" if passed a boolean', () => {
+    const input = true;
+    const expectedOutput = "Error: Argument is not a string";
+    assert.deepEqual(countLetters(input), expectedOutput);
+  })
+
+  it('should return "Error: Argument is not a string" if passed undefined', () => {
+    const input = undefined;
+    const expectedOutput = "Error: Argument is not a string";
+    assert.deepEqual(countLetters(input), expectedOutput);
+  })
+
+  it('should return "Error: Argument is not a string" if passed null', () => {
+    const input = null;
+    const expectedOutput = "Error: Argument is not a string";
+    assert.deepEqual(countLetters(input), expectedOutput);
+  })
+
+  it('should return "Error: String has no characters within" if passed ""', () => {
+    const input = "";
+    const expectedOutput = "Error: String has no characters within";
+    assert.deepEqual(countLetters(input), expectedOutput);
+  });
+
+})
