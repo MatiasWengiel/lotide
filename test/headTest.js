d0a140abe1aae2cbed405af511c2e6a11ef37946
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-13 11:27:40 -0500

    refactored to use constants for the assertion rather than passing the input and expected values directly

--- test/headTest.js
+++ test/headTest.js
@@ -1,41 +1,46 @@
-// const assertEqual = require('../assertEqual');
-
 const assert = require('chai').assert;
 const head = require('../head');
 
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head(["Test"]), "Test");
-// assertEqual(head("test"), "test");
-// assertEqual(head([]), undefined);
-// assertEqual(head(5), 5);
-
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(head([1, 2, 3]), 1);
+    const inputArray = [1, 2, 3];
+    const expectedOutput = 1;
+    assert.strictEqual(head(inputArray), expectedOutput);
   });
 
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5');
+    const inputArray = ['5'];
+    const expectedOutput = '5'
+    assert.strictEqual(head(inputArray), expectedOutput);
   });
 
   it("returns 5 (as number) for [5]", () => {
-    assert.strictEqual(head([5]), 5);
+    const inputArray = [5];
+    const expectedOutput = 5;
+    assert.strictEqual(head(inputArray), expectedOutput);
   });
 
   it("returns [false] (as a boolean) as false (as a boolean)", () => {
-    assert.strictEqual(head([false]), false);
+    const inputArray = [false];
+    const expectedOutput = false;
+    assert.strictEqual(head(inputArray), expectedOutput);
   });
 
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+    const inputArray = ['Hello', 'Lighthouse', 'Labs'];
+    const expectedOutput = 'Hello'
+    assert.strictEqual(head(inputArray), expectedOutput);
   });
 
   it("returns 'The argument is not an array' for ['test']", () => {
-    assert.strictEqual(head('test'), 'The argument is not an array')
+    const inputString = 'test';
+    const expectedOutput = 'The argument is not an array'
+    assert.strictEqual(head(inputString), expectedOutput);
   })
 
   it("returns undefined for []", () => {
-    assert.strictEqual(head([]), undefined);
+    const inputArray = [];
+    const expectedOutput = undefined;
+    assert.strictEqual(head(inputArray), expectedOutput);
   });
 });
