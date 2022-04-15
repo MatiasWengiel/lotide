ac62d19e6b4608affc9e2188bf3229bed28ba595
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-15 15:56:35 -0500

    tests now pass for multiple nested arrays. Also fixed linting erros

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -7,50 +7,57 @@ describe('#takeUntil', () => {
     const expectedOutput = [1, 2, 3];
     const callbackFn = x => x > 3;
     assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput);
-  })
+  });
 
   it('should return [] for array [1, 2, 3, 4, 5] and callback x => x < 3', () => {
     const inputArr = [1, 2, 3, 4, 5];
     const expectedOutput = [];
     const callbackFn = x => x < 3;
     assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput);
-  })
+  });
 
   it('should return  for ["I", "think", ",", "therefore", "I", "am"] and callback x => x === "," ', () => {
     const inputArr = ["I", "think", ",", "therefore", "I", "am"];
     const expectedOutput = ["I", "think"];
     const callbackFn = x => x === ",";
     assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput);
-  })
+  });
 
   it('should return [false] for array [false, true, false] and callback x => x === !false', () => {
     const inputArr = [false, true, false];
-    const expectedOutput = [false]
+    const expectedOutput = [false];
     const callbackFn = x => x === !false;
-    assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput)
-  })
+    assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput);
+  });
 
   it('should return [1, 2] for array [1, 2, [3, 4]] and callback x => Array.isArray(x)', () => {
     const inputArr = [1, 2, [3, 4]];
-    const expectedOutput = [1, 2]
+    const expectedOutput = [1, 2];
     const callbackFn = x => Array.isArray(x);
-    assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput)
-  })
+    assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput);
+  });
 
   it('should return ["LHL", 15] for array ["LHL", 15, true, "fun"] and callback x === true', () => {
-    const inputArr = ["LHL", 15, true, "fun"] ;
-    const expectedOutput = ["LHL", 15]
+    const inputArr = ["LHL", 15, true, "fun"];
+    const expectedOutput = ["LHL", 15];
     const callbackFn = x => x === true;
-    assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput)
-  })
+    assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput);
+  });
 
   it('should return [1, [2]] for [1, [2, 3]] and callback x => x >= 3', () => {
     const inputArr = [1, [2, 3]];
     const expectedOutput = [1, [2]];
     const callbackFn = x => x >= 3;
-    assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput)
-  })
-})
+    assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput);
+  });
+
+  it('should return [1, [2, [3, [4]]]] for [1, [2, [3, [4, 5]]]] and callback x => x === 5', () => {
+    const inputArr = [1, [2, [3, [4, 5]]]];
+    const expectedOutput = [1, [2, [3, [4]]]];
+    const callbackFn = x => x === 5;
+    assert.deepEqual(takeUntil(inputArr, callbackFn), expectedOutput);
+  });
+});
 
 // console.log("should pass \n");
 // const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
