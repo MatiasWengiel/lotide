9f221670ff32548b369c68782f007a8d6600a79b
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-15 15:02:26 -0500

    Removed unnecessary spacing

--- letterPositions.js
+++ letterPositions.js
@@ -2,12 +2,12 @@ const letterPositions = function(sentence) {
   if (typeof sentence !== "string") { //ensure we are dealing with a string
     return "Error: Argument is not a string";
   }
+
   const lowerCaseNoSpaces = sentence.split(" ").join("").toLowerCase();
   const results = {};
 
-
   for (let i = 0; i < lowerCaseNoSpaces.length; i++) {
-  //Check if each letter in lowerCaseNoSpaces is present as a key in results:
+  //Checks if each letter in lowerCaseNoSpaces is present as a key in results:
   // if it is not, create an array with the key of the letter at i and the value of i
   // if it is, push i to the existing array
     results[lowerCaseNoSpaces[i]] ? results[lowerCaseNoSpaces[i]].push(i) : results[lowerCaseNoSpaces[i]] = [i];
@@ -16,4 +16,3 @@ const letterPositions = function(sentence) {
 };
 
 module.exports = letterPositions;
-
