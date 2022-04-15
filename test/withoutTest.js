3915db911b56a8932adc3b16558f607cffbeed51
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-15 16:15:14 -0500

    fixed linting errors

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -8,51 +8,39 @@ describe('#without', () => {
     const expectedOutput = [];
     const withoutParam = [1];
     assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
-  })
+  });
   it('should return [1, 2] for [1, 2, 3] and without parameter [3]', () => {
     const inputArr = [1, 2, 3];
     const expectedOutput = [1, 2];
     const withoutParam = [3];
     assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
-  })
+  });
 
   it('should return [1, 2, 4] for [1, 2, 3, 4] with parameter [3]', () => {
     const inputArr = [1, 2, 3, 4];
     const expectedOutput = [1, 2, 4];
     const withoutParam = [3];
     assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
-  })
+  });
 
   it('should return ["eggs", "and", "ham"] for ["green", "eggs", "and", "ham"] and parameter ["green]', () => {
     const inputArr = ["green", "eggs", "and", "ham"];
     const expectedOutput = ["eggs", "and", "ham"];
     const withoutParam = ["green"];
     assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
-  })
+  });
 
   it('should return [false, false] for [false, true, false] and parameter [true]', () => {
     const inputArr = [false, true, false];
     const expectedOutput = [false, false];
     const withoutParam = [true];
     assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
-  })
+  });
 
-  it ('should return [1, "and", undefined] for [1, true, "and", false, undefined] and parameter [true, false]', () => {
+  it('should return [1, "and", undefined] for [1, true, "and", false, undefined] and parameter [true, false]', () => {
     const inputArr = [1, true, "and", false, undefined];
     const expectedOutput = [1, "and", undefined];
-    const withoutParam = [true, false]
+    const withoutParam = [true, false];
     assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
-  })
-})
-
-
-// assertArraysEqual(without([1, 2, 3], [1, 2]), [3]);
-// assertArraysEqual(without([1, 2, 3], [2]), [1, 3]);
-// assertArraysEqual(without([1, 2, 3], [1, "2", true, 3]), [2]);
-// assertArraysEqual(without([1, 2, 3], []), [1, 2, 3]);
-// assertArraysEqual(without([1, 2, 3], [true]), [1, 2, 3]);
-// assertArraysEqual(without([1, 2, 3], [false]), [1, 2, 3]);
-// assertArraysEqual(without([1, 2, 3], ["1, 2, 3"]), [1, 2, 3]);
-// assertArraysEqual(without([1, 1, 2, 3], [1]), [2, 3]);
-// assertArraysEqual(without(["green", "eggs", "and", "ham"], ["eggs"]), ["green", "and", "ham"]);
-// assertArraysEqual(without([true, false, 1, "banana"], [true]), [false, 1, "banana"]);
+  });
+});
