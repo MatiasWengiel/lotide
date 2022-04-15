584bbb8b3472a6ed66cfa6180e2daf1768b7e3ee
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-15 16:08:47 -0500

    testing now passes for removing numbers in the middle of an array, as well as removing strings from an array of strings

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -8,6 +8,20 @@ describe('#without', () => {
     const withoutParam = [3];
     assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
   })
+
+  it('should return [1, 2, 4] for [1, 2, 3, 4] with parameter [3]', () => {
+    const inputArr = [1, 2, 3, 4];
+    const expectedOutput = [1, 2, 4];
+    const withoutParam = [3];
+    assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
+  })
+
+  it('should return ["eggs", "and", "ham"] for ["green", "eggs", "and", "ham"] and parameter ["green]', () => {
+    const inputArr = ["green", "eggs", "and", "ham"];
+    const expectedOutput = ["eggs", "and", "ham"];
+    const withoutParam = ["green"];
+    assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
+  })
 })
 // assertArraysEqual(without([1, 2, 3], [1, 2]), [3]);
 // assertArraysEqual(without([1, 2, 3], [2]), [1, 3]);
