e0fe6b33e6c83da2a9c9d11dece28a1e25904b24
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-15 11:54:23 -0500

    fixed linting errors

--- flatten.js
+++ flatten.js
@@ -1,10 +1,10 @@
 const flatten = (array) => {
-  let outputArr = []
+  let outputArr = [];
   for (let eachIndex of array) {
     if (Array.isArray(eachIndex)) {
       let subOut = flatten(eachIndex);
       for (let eachSubIndex of subOut) {
-        outputArr.push(eachSubIndex)
+        outputArr.push(eachSubIndex);
       }
     } else {
       outputArr.push(eachIndex);
@@ -12,5 +12,5 @@ const flatten = (array) => {
   }
   
   return outputArr;
-}
+};
 module.exports = flatten;
