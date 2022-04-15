9dbb56a6c9664a04ab6f829c87650ad71d117736
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-15 16:14:30 -0500

    testing now passes for arrays of numbers, strings, booleans and mixed types. Also passes for removing multiple parameters at once

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -2,6 +2,13 @@ const without = require('../without');
 const assert = require('chai').assert;
 
 describe('#without', () => {
+
+  it('should return [] for [1] and without parameter [1]', () => {
+    const inputArr = [1];
+    const expectedOutput = [];
+    const withoutParam = [1];
+    assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
+  })
   it('should return [1, 2] for [1, 2, 3] and without parameter [3]', () => {
     const inputArr = [1, 2, 3];
     const expectedOutput = [1, 2];
@@ -22,7 +29,23 @@ describe('#without', () => {
     const withoutParam = ["green"];
     assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
   })
+
+  it('should return [false, false] for [false, true, false] and parameter [true]', () => {
+    const inputArr = [false, true, false];
+    const expectedOutput = [false, false];
+    const withoutParam = [true];
+    assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
+  })
+
+  it ('should return [1, "and", undefined] for [1, true, "and", false, undefined] and parameter [true, false]', () => {
+    const inputArr = [1, true, "and", false, undefined];
+    const expectedOutput = [1, "and", undefined];
+    const withoutParam = [true, false]
+    assert.deepEqual(without(inputArr, withoutParam), expectedOutput);
+  })
 })
+
+
 // assertArraysEqual(without([1, 2, 3], [1, 2]), [3]);
 // assertArraysEqual(without([1, 2, 3], [2]), [1, 3]);
 // assertArraysEqual(without([1, 2, 3], [1, "2", true, 3]), [2]);
