527daec2bb63720f4ceede642245d45c8aff190d
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-15 10:27:05 -0500

    Moved testing functions to a separate file

--- flatten.js
+++ flatten.js
@@ -1,26 +1,3 @@
-// const eqArrays = function(arrayOne, arrayTwo) {
-//   if (arrayOne.length !== arrayTwo.length) {
-//     return false;
-//   }
-
-//   if (arrayOne.length === 0 && arrayTwo.length === 0) {
-//     return true;
-//   }
-
-//   for (let i = 0; i < arrayOne.length; i++) {
-//     if (arrayOne[i] !== arrayTwo[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
-
-// const assertArraysEqual = function(arrayOne, arrayTwo) {
-
-//   eqArrays(arrayOne, arrayTwo) ? console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${arrayOne} === ${arrayTwo}. ✅`) :
-//     console.log("\x1b[31m%s\x1b[0m", `🚩 Assertion Failed: ${arrayOne} !== ${arrayTwo}. 🚩`);
-// };
-
 const flatten = function(array) {
   let flattenedArray = [];
   for (let i = 0; i < array.length; i++) {
@@ -36,10 +13,3 @@ const flatten = function(array) {
 };
 
 module.exports = flatten;
-
-// assertArraysEqual(flatten([]), []);
-// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
-// assertArraysEqual(flatten(["fox", ["bear", "rabbit"], "elephant"]), ["fox", "bear", "rabbit", "elephant"]);
-// assertArraysEqual(flatten([2, 3, 4]), [2, 3, 4]);
-// assertArraysEqual(flatten([2, "fox", true, [true, false], "bear"]), [2, "fox", true, true, false, "bear"]);
-// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 7]); // false, should fail
