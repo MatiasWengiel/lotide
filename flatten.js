4f0d1a5a762502a5d74400d9ab68114695ff1915
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-15 11:45:10 -0500

    function now flattens any number of nested arrays using recursion

--- flatten.js
+++ flatten.js
@@ -1,28 +1,16 @@
-// const flatten = function(array) {
-//   let flattenedArray = [];
-//   for (let i = 0; i < array.length; i++) {
-//     if (!(Array.isArray(array[i]))) {
-//       flattenedArray.push(array[i]);
-//     } else {
-//       for (let item of array[i]) {
-//         flattenedArray.push(item);
-//       }
-//     }
-//   }
-//   return flattenedArray;
-// };
-
-// base case: Array.isArray(index) === false
-
 const flatten = (array) => {
   let outputArr = []
   for (let eachIndex of array) {
-    if (!(Array.isArray(eachIndex))){
+    if (Array.isArray(eachIndex)) {
+      let subOut = flatten(eachIndex);
+      for (let eachSubIndex of subOut) {
+        outputArr.push(eachSubIndex)
+      }
+    } else {
       outputArr.push(eachIndex);
     }
   }
+  
   return outputArr;
 }
-
-
 module.exports = flatten;
