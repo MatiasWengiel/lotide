f6d84742eef1cdca949bcfeb666063a86daf6b94
Matias Wengiel <matiaswengiel@gmail.com>
2022-04-15 11:55:27 -0500

    flatten now passes tests for  many levels of recursion, strings, numbers, booleans, undefined and null

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -10,25 +10,44 @@ describe('#flatten', () => {
 
   it('should return the same array if passed an array with no nesting, such as [1, 2, 3, 4] for [1, 2, 3, 4]', () => {
     const inputArr = [1, 2, 3, 4];
-    const expectedOutput = [1, 2, 3, 4]
+    const expectedOutput = [1, 2, 3, 4];
     assert.deepEqual(flatten(inputArr), expectedOutput);
-  })
+  });
 
   it('should return a flat array if passed an array with nested arrays within, such as [1, 2, 3, 4] for [1, 2, [3, 4]]', () => {
     const inputArr = [1, 2, [3, 4]];
-    const expectedOutput = [1, 2, 3, 4]
+    const expectedOutput = [1, 2, 3, 4];
     assert.deepEqual(flatten(inputArr), expectedOutput);
-  })
+  });
 
   it('should return a flat array even if passed multiple levels of nesting, such as [1, 2, 3, 4, 5,6] for [1, [2, [3, [4, [5, 6]]]]]', () => {
     const inputArr = [1, [2, [3, [4, [5, 6]]]]];
     const expectedOutput = [1, 2, 3, 4, 5, 6];
     assert.deepEqual(flatten(inputArr), expectedOutput);
-  })
-})
-// assertArraysEqual(flatten([]), []);
-// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
-// assertArraysEqual(flatten(["fox", ["bear", "rabbit"], "elephant"]), ["fox", "bear", "rabbit", "elephant"]);
-// assertArraysEqual(flatten([2, 3, 4]), [2, 3, 4]);
-// assertArraysEqual(flatten([2, "fox", true, [true, false], "bear"]), [2, "fox", true, true, false, "bear"]);
-// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 7]); // false, should fail
+  });
+
+  it('should return a flat array even if passed strings such as ["test", "Lighthouse", "Labs"] for  ["test", ["Lighthouse", ["Labs"]]]', () => {
+    const inputArr = ["test", ["Lighthouse", ["Labs"]]];
+    const expectedOutput = ["test", "Lighthouse", "Labs"];
+    assert.deepEqual(flatten(inputArr), expectedOutput);
+  });
+
+  it('should return a flat array even if passed booleans, such as [false, true, false] for [false, [true, [false]]]', () => {
+    const inputArr = [false, [true, [false]]];
+    const expectedOutput = [false, true, false];
+    assert.deepEqual(flatten(inputArr), expectedOutput);
+  });
+
+  it('should return a flat array even if passed null or undefined, such as [null, undefined, null, undefined] for [null, [undefined, [null, [undefined]]]]', () => {
+    const inputArr = [null, [undefined, [null, [undefined]]]];
+    const expectedOutput = [null, undefined, null, undefined];
+    assert.deepEqual(flatten(inputArr), expectedOutput);
+  });
+
+  it('should return a flat array even if passed mixed strings, numbers, booleans, undefined or null within nested arrays', () => {
+    const inputArr = ["one", true, [false, [undefined, 3], [null]]];
+    const expectedOutput = ["one", true, false, undefined, 3, null];
+    assert.deepEqual(flatten(inputArr), expectedOutput);
+  });
+
+});
